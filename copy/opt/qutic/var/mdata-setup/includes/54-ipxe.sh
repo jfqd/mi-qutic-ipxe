d9d3091f8427c162ef491cb53f5977f9aef8cded
jfqd <jfqd@blun.org>
2020-11-27 20:31:03 +0100

    remove unwanted char

--- copy/opt/qutic/var/mdata-setup/includes/54-ipxe.sh
+++ copy/opt/qutic/var/mdata-setup/includes/54-ipxe.sh
@@ -6,7 +6,7 @@ SUBNET=$(echo ${IP} | cut -d"." -f1-3)
 sed -i \
     -e "s/next-server 10.10.10.58;/next-server ${IP};/" \
     -e "s|subnet 10.10.10.0|subnet ${SUBNET}.0|" \
-    -e "s|range 10.10.10.240 10.10.10.249;|range 1${SUBNET}.240 ${SUBNET}.249;|" \
+    -e "s|range 10.10.10.240 10.10.10.249;|range ${SUBNET}.240 ${SUBNET}.249;|" \
     /opt/local/etc/dhcp/dhcpd.conf
 svcadm enable svc:/pkgsrc/isc-dhcpd:default
 # svcadm restart svc:/pkgsrc/isc-dhcpd:default
